18838e15e56c29d3b95046f1c457fa10309c779d
abder45400 <97219626+abder45400@users.noreply.github.com>
2022-02-07 15:59:30 +0100

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+def on_mes_dpad_controller_id_microbit_evt():
+    global lastValue, vitesseM2, vitesseM1
+    if lastValue != control.event_value():
+        lastValue = control.event_value()
+        vitesseM2 = 255
+        vitesseM1 = 255
+        led.stop_animation()
+        if control.event_value() == 1:
+           basic.show_string("A")
+        elif control.event_value() == 3:
+           basic.show_string("B")
+        elif control.event_value() == 5:
+            basic.show_string("C")
+        elif control.event_value() == 7:
+            basic.show_string("D")
+        elif control.event_value() == 9:
+            basic.show_string("1")
+        else:
+            basic.clear_screen()
 
+vitesseM1 = 0
+vitesseM2 = 0
+lastValue = 0
+lastValue = 0
+vitesseM2 = 0
+vitesseM1 = 0
+basic.show_string("Hello!")
--- main.ts
+++ main.ts
@@ -1,33 +1,33 @@
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (lastValue != control.eventValue()) {
         lastValue = control.eventValue()
-        vitesseM1 = 255
         vitesseM2 = 255
-    } else if (control.eventValue() == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.showString("Avant")
-    } else if (control.eventValue() == 3) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.showString("Arrière")
-    } else if (control.eventValue() == 5) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
-        basic.showString("droite")
-    } else if (control.eventValue() == 7) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-        basic.showString("gauche")
-    } else {
-        maqueen.motorStop(maqueen.Motors.All)
-        basic.showString("Stop")
+        vitesseM1 = 255
+        if (control.eventValue() == 1) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            basic.showString("A")
+        } else if (control.eventValue() == 3) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            basic.showString("B")
+        } else if (control.eventValue() == 5) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            basic.showString("C")
+        } else if (control.eventValue() == 7) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            basic.showString("D")
+        } else if (control.eventValue() == 9) {
+            maqueen.motorStop(maqueen.Motors.All)
+        }
     }
 })
-let vitesseM1 = 0
 let vitesseM2 = 0
+let vitesseM1 = 0
 let lastValue = 0
+basic.showIcon(IconNames.Yes)
 lastValue = 0
-vitesseM2 = 0
 vitesseM1 = 0
-basic.showString("Hello!")
+vitesseM2 = 0
