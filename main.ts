9defbd7a38cd2ccd5fdd3ff72ee134136c73e203
abder45400 <97219626+abder45400@users.noreply.github.com>
2022-02-07 16:02:11 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,20 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         vitesseM2 = 255
         vitesseM1 = 255
         if (control.eventValue() == 1) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, vitesseM1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesseM2)
             basic.showString("A")
         } else if (control.eventValue() == 3) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesseM1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, vitesseM2)
             basic.showString("B")
         } else if (control.eventValue() == 5) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, vitesseM1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, vitesseM2)
             basic.showString("C")
         } else if (control.eventValue() == 7) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, vitesseM1)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, vitesseM2)
             basic.showString("D")
         } else if (control.eventValue() == 9) {
             maqueen.motorStop(maqueen.Motors.All)
